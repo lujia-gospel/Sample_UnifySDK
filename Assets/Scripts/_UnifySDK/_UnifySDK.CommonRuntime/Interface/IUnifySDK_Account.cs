0cc1cc655a01d78d29238a26341e96bd8f422b18
jianglj <>
2023-07-21 19:29:37 +0800

    修改结构

--- Assets/Scripts/_UnifySDK/_UnifySDK.CommonRuntime/Interface/IUnifySDK_Account.cs
+++ Assets/Scripts/_UnifySDK/_UnifySDK.CommonRuntime/Interface/IUnifySDK_Account.cs
@@ -5,6 +5,6 @@ namespace UnifySDK
     [UnifySDKInterface]
     public interface IUnifySDK_Account
     {
-        public AEvent<UnifySDKEvent.InitSuccessData> OnInitSuccess { get; set; }
+       
     }
 }
