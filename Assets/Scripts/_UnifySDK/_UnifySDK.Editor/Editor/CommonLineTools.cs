7a3ad145347c5c8909cc876c9d371b4257216cda
jianglj <>
2023-12-06 16:00:41 +0800

    [4wd/release]删除无用文件 ，添加python文件

---
+++ Assets/Scripts/_UnifySDK/_UnifySDK.Editor/Editor/CommonLineTools.cs
@@ -0,0 +1,223 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnifySDK.Editor;
+
+public static class CommonLineTools 
+{
+	[MenuItem("Tools/UnifySDK/UnifyExtendGitReset", false, 0)]
+	public static void UnifyExtendGitReset()
+	{
+		var resetPath = $"{Application.dataPath}/Scripts/_UnifySDK/_UnifySDK.Extend";
+		CommandLineUtils.RunDirectly("git", $"checkout -- {resetPath}", Application.dataPath);
+	}
+	[MenuItem("Tools/UnifySDK/根据环境变量删除SDK", false, 0)]
+	public static void UnifySDKBuild()
+	{
+		var py = "../Tools/UnifySDKTool/UnifySDKTool.py";
+		var model = PlayerPrefs.GetString(EnvironmentVariableSettingsEditor.LocalUnifySDKRecord, "None");
+		CommandLineUtils.RunDirectly("Python 3", $"{py} {model}", Application.dataPath);
+	}
+}
+
+
+public static class CommandLineUtils  
+{	
+
+	const int kMaxThreads = 4;
+	
+	public class AsyncTask
+	{
+		public string command;
+		public string args;
+		public string dir;
+		public string result;
+		public bool   done;
+	};
+	static Queue<AsyncTask> _queue = new Queue<AsyncTask>(1024);
+	static System.Threading.Semaphore _semaphore = new System.Threading.Semaphore(0,kMaxThreads);
+	static bool _running = false;
+	
+	
+	static void _Thread()
+	{
+		while (_running)
+		{
+			while(true)
+			{
+				AsyncTask task = null;
+				
+				lock(_queue)
+				{
+					if ( _queue.Count > 0 )
+					{
+						task = _queue.Dequeue();
+					}
+				}
+				
+				if ( task == null )
+				{
+					break;
+				}
+				
+				task.result = Run( task.command, task.args, task.dir ); 
+				task.done = true;
+			}
+		
+			_semaphore.WaitOne();
+		}
+	}
+	
+	static System.Diagnostics.Process _RunAsync(string command, string arguments, string workingDir) 
+	{
+
+		//Debug.Log("Runing: " + command + " " + arguments);
+		//      System.Diagnostics.Process p = new System.Diagnostics.Process();
+		//p.StartInfo.FileName = command;
+		//p.StartInfo.Arguments = arguments;
+		//p.StartInfo.WorkingDirectory = workingDir;
+		//p.StartInfo.UseShellExecute = true;
+		//p.StartInfo.CreateNoWindow = false;			
+
+		//p.Start();
+		//return p;
+		var pStartInfo = new System.Diagnostics.ProcessStartInfo(command);
+		pStartInfo.Arguments = arguments;
+		pStartInfo.CreateNoWindow = false;
+		pStartInfo.UseShellExecute = true;
+		pStartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+		pStartInfo.RedirectStandardError = false;
+		pStartInfo.RedirectStandardInput = false;
+		pStartInfo.RedirectStandardOutput = false;
+		if (!string.IsNullOrEmpty(workingDir))
+			pStartInfo.WorkingDirectory = workingDir;
+	   return System.Diagnostics.Process.Start(pStartInfo);
+	}
+	
+	static int _Run(string command, string arguments, string workingDir) 
+	{
+		var p = _RunAsync(command, arguments, workingDir);
+		p.WaitForExit();
+		//Debug.Log("Run done " + p.ExitCode);
+		var code = p.ExitCode;
+		p.Dispose();
+		System.GC.Collect();
+		return code;
+	}
+	
+	static void _DeleteFile(string file)
+	{
+		try
+		{
+			File.Delete(file);
+		}
+		catch {}
+	}
+	
+	public static AsyncTask RunAsync( string command, string arguments )
+	{
+		return RunAsync( command, arguments, Directory.GetCurrentDirectory() );	
+	}
+	
+	public static AsyncTask RunAsync( string command, string arguments, string workingDir )
+	{
+		if ( _running == false )
+		{
+			_running = true;
+			for ( int i = 0; i < kMaxThreads; ++i )
+			{
+				new System.Threading.Thread(_Thread).Start();
+			}
+		}
+		
+		AsyncTask task = new AsyncTask();
+		task.command = command;
+		task.args = arguments;
+		task.dir = workingDir;
+		lock(_queue)
+		{
+			_queue.Enqueue(task);
+		}
+		
+		// wake up threads
+		try
+		{
+			_semaphore.Release();
+		}
+		catch {}
+		
+		return task;
+	}
+	
+	public static void WaitForTasks()
+	{
+		while( true )
+		{
+			bool done = true;
+			lock(_queue)
+			{
+				done = _queue.Count == 0;
+			}
+			
+			if ( done )
+			{
+				return;
+			}
+			
+			System.Threading.Thread.Sleep(100);
+		}
+	}
+	
+	public static string Run(string command, string arguments)
+	{
+		return Run(command,arguments,Directory.GetCurrentDirectory());
+	}
+		
+	public static string Run(string command, string arguments, string workingDir) 
+	{
+		Debug.Log("Running: " + command + " " + arguments);
+	
+		int hash		= System.Diagnostics.Process.GetCurrentProcess().Id;
+		int threadId	= System.Threading.Thread.CurrentThread.ManagedThreadId;
+		
+		string file 	= string.Format("/tmp/unity_command_{0}_{1}.sh", hash, threadId);
+		string outfile 	= string.Format("/tmp/unity_command_{0}_{1}out.txt", hash, threadId);
+		string script 	= string.Format("#!/bin/bash\ntouch {2}\n{0} {1} > {2}\n", command, arguments, outfile);
+		_DeleteFile(outfile);
+		File.WriteAllText(file, script);
+		_Run("chmod", "u+x " + file, workingDir);
+		_Run(file, string.Empty, workingDir);
+		
+		string output = string.Empty;
+		try
+		{
+			output = File.ReadAllText(outfile);
+            Debug.Log(@" Running log:  {output} ");
+		}
+		catch {}
+		
+        if(output.Contains("xcodebuild: error:"))
+        {
+	        Debug.LogError($" Running log error::  {output} ");
+            throw new Exception(output);
+        }
+
+        _DeleteFile(script);
+		_DeleteFile(outfile);
+		
+		return output;
+	}
+
+    public static void RunDirectly(string command, string arguments, string workingDir)
+    {
+        _Run(command, arguments, workingDir);
+    }
+
+    public static void RunDirectly(string command)
+    {
+        int code = _Run(command, "", Directory.GetCurrentDirectory());
+    }
+
+}
---
+++ Tools/UnifySDKTool/UnifySDKTool.py
@@ -0,0 +1,146 @@
+#!usr/bin/env python
+import os
+import shutil
+import sys
+import json
+
+UnityPath = ''
+UnityDataPath = ''
+ExternalRootPath = ''
+
+root_path = os.path.abspath(os.path.dirname(__file__))
+UnityPath = root_path.replace("\\Tools\\UnifySDKTool", "")
+UnityPath = UnityPath.replace("/Tools/UnifySDKTool", "")
+UnityDataPath = os.path.join(UnityPath, "Assets")
+ExternalRootPath = os.path.join(UnityPath, "Tools/SdkFrame")
+print("root_path:"+root_path)
+print("UnityPath:"+UnityPath)
+print("UnityDataPath:"+UnityDataPath)
+print("ExternalRootPath:"+ExternalRootPath)
+
+def write_file_string(path, data):
+    p = os.path.dirname(path)
+    if not os.path.exists(p):
+        os.makedirs(p,True)
+    with open(path, 'w', encoding='utf-8') as file:
+        file.write(data)
+
+def read_file_string(path):
+    if os.path.exists(path):
+        with open(path, 'r', encoding='utf-8') as file:
+            return file.read()
+    return ''
+
+def plugins_move_to_project(file_name, path):
+    source_path = f"{UnityDataPath}/{path}/Plugins" 
+    target_path = f"{UnityDataPath}/Plugins"  
+    source_dire_info = os.path.abspath(source_path)
+    dest_dire = os.path.abspath(target_path)
+    #print(f"  CopyDireToDire  sourcePath: {source_path} targetPath: {target_path}  ")
+    print(f"  CopyDireToDire  sourceDireInfo: {source_dire_info} destDire: {dest_dire}  ")
+    merge_folders(source_dire_info, dest_dire)
+
+def delete_plugins_folder(target_path):
+    # 使用shutil.rmtree()
+    if os.path.isdir(target_path):
+        shutil.rmtree(target_path)
+
+def merge_folders(source_folder, destination_folder):
+    for root, dirs, files in os.walk(source_folder):
+        # 构造目标文件夹路径
+        destination_root = os.path.join(destination_folder, os.path.relpath(root, source_folder))
+
+        # 如果目标文件夹不存在，则创建
+        if not os.path.exists(destination_root):
+            os.makedirs(destination_root)
+
+        # 移动文件
+        for file in files:
+            source_path = os.path.join(root, file)
+            destination_path = os.path.join(destination_root, file)
+            shutil.move(source_path, destination_path)
+    print("文件夹内容已成功移动到目标路径，并合并同名文件夹。")
+    delete_plugins_folder(source_folder)
+
+def delete_dire_fun(delete_dire):
+    if os.path.isfile(delete_dire):
+        os.remove(delete_dire)
+        delete_dire_meta = delete_dire + ".meta"
+        if os.path.isfile(delete_dire_meta):
+            os.remove(delete_dire_meta)
+    if os.path.isdir(delete_dire):
+        shutil.rmtree(delete_dire)
+        delete_dire_meta = delete_dire + ".meta"
+        if os.path.isfile(delete_dire_meta):
+            os.remove(delete_dire_meta)
+
+def logic(args):
+    for arg in args:
+        print(f"UnifySDKTool SDK Logic {arg}")
+
+    SDKListTemplatePath = f"{UnityPath}/Assets/3rdPlugins/_UnifySDK/_UnifySDK.Runtime/Core/ConfigSetting/Resources/SDKListTemplate.txt"
+    print(f"SDKListTemplatePath: {SDKListTemplatePath}")
+
+    AssetConfigPaths = f"{UnityPath}/Assets/3rdPlugins/_UnifySDK/_UnifySDK.Editor/Editor/ConfigSetting/Resources"
+    SDKListModel = None
+    CustomSDKList = None
+    if len(args) > 0:
+        SDKListModel = args[0]
+        print(f"args[0]: {args[0]}")  
+    if len(args) > 1:
+        CustomSDKList = args[1]
+        print(f"args[1]: {args[1]}")  
+
+    sdkModelName = "None"
+    
+    with open(SDKListTemplatePath, "r") as file:
+        SDKListTemplate = file.read()
+    sdkModelDic = json.loads(SDKListTemplate)
+    needSDKList = []
+
+    if SDKListModel is not None and SDKListModel not in sdkModelDic:
+        print(f"SDKListTemplate 不包含该值 {SDKListModel} （Jenkins SDKListModel）")
+    elif SDKListModel in sdkModelDic:
+        needSDKList = sdkModelDic[SDKListModel].keys()
+
+    assetConfigFiles = [file for file in os.listdir(AssetConfigPaths) if file.endswith(".asset")]
+
+    for file in assetConfigFiles:
+        filePath = os.path.join(AssetConfigPaths, file)
+        sdkName,_ = os.path.splitext(file)
+        if sdkName not in needSDKList:
+        
+            with open(filePath, "r") as assetFile:
+                assetConfig = assetFile.read()
+            
+            list = assetConfig.split("  - assetFolder: ")[1:]
+		      
+            for item in list:
+                item = item.replace("\n", "").replace("\r", "")
+                targetPath = os.path.join(UnityDataPath, item)
+                delete_dire_fun(targetPath)
+    delet_path = f"{UnityDataPath}/Plugins" 
+    delete_plugins_folder(delet_path)
+    for file in assetConfigFiles:
+        sdkName,_ = os.path.splitext(file)
+
+        if sdkName in needSDKList:
+            filePath = os.path.join(AssetConfigPaths, file)
+            with open(filePath, "r") as assetFile:
+                assetConfig = assetFile.read()
+
+            list = assetConfig.split("  - assetFolder: ")[1:]
+
+            for item in list:
+                item = item.replace("\n", "").replace("\r", "")
+                plugins_move_to_project(sdkName, item)
+
+def main(args):
+    if len(args) > 0:
+        logic(args)    
+    else:
+        print(f"args count == 0")
+		
+		
+if __name__ == "__main__":
+    main(sys.argv[1:]) 
